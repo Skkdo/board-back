caf661671b6985738abc7598278c4662b1f9225e
JinHyoung <104055567+Skkdo@users.noreply.github.com>
2024-09-25 21:20:50 +0900

    feat account log

--- src/main/java/com/kjh/boardback/entity/board/BoardEntity.java
+++ src/main/java/com/kjh/boardback/entity/board/BoardEntity.java
@@ -2,11 +2,7 @@ package com.kjh.boardback.entity.board;
 
 import com.kjh.boardback.dto.request.board.PatchBoardRequestDto;
 import com.kjh.boardback.dto.request.board.PostBoardRequestDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,8 +17,9 @@ import java.util.Date;
 @Entity(name = "board")
 @Table(name = "board")
 public class BoardEntity {
-    
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int boardNumber;
 
     private String title;
@@ -30,7 +27,7 @@ public class BoardEntity {
     private String content;
 
     private String writeDatetime;
-    
+
     private int favoriteCount;
 
     private int commentCount;
@@ -40,21 +37,7 @@ public class BoardEntity {
     private String writerEmail;
 
 
-
-    public void increaseViewCount(){
-        this.viewCount++;
-    }
-
-    public void increaseCommentCount(){
-        this.commentCount++;
-    }
-
-    public void decreaseCommentCount() {this.commentCount--;}
-
-    public void increaseFavoriteCount(){this.favoriteCount++;}
-    public void decreaseFavoriteCount(){this.favoriteCount--;}
-
-    public BoardEntity(PostBoardRequestDto dto,String email) {
+    public BoardEntity(PostBoardRequestDto dto, String email) {
 
         Date now = Date.from(Instant.now());
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -63,13 +46,33 @@ public class BoardEntity {
         this.title = dto.getTitle();
         this.content = dto.getContent();
         this.writeDatetime = writeDateTime;
-        this.favoriteCount =0;
+        this.favoriteCount = 0;
         this.commentCount = 0;
         this.viewCount = 0;
         this.writerEmail = email;
     }
 
-    public void patchBoard(PatchBoardRequestDto dto){
+    public void increaseViewCount() {
+        this.viewCount++;
+    }
+
+    public void increaseCommentCount() {
+        this.commentCount++;
+    }
+
+    public void decreaseCommentCount() {
+        this.commentCount--;
+    }
+
+    public void increaseFavoriteCount() {
+        this.favoriteCount++;
+    }
+
+    public void decreaseFavoriteCount() {
+        this.favoriteCount--;
+    }
+
+    public void patchBoard(PatchBoardRequestDto dto) {
         this.title = dto.getTitle();
         this.content = dto.getContent();
     }
