ed5c66894ed3228d89ddacfc3310b746a4cbf4d7
박진형 <skill1218@naver.com>
2025-01-26 15:01:00 +0900

    fix

--- src/test/java/board/global/security/AuthenticationPrincipalTest.java
+++ src/test/java/board/global/security/AuthenticationPrincipalTest.java
@@ -11,10 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest(classes = BoardBackApplication.class)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class AuthenticationPrincipalTest {
 
